25439a2dffadc30476a120f2e0f81a74b9f7097d
duda <mariaedrdabreu@gmail.com>
2025-07-28 20:22:54 -0300

    removendo espaco extra

--- 02_model_serving_api/main.py
+++ 02_model_serving_api/main.py
@@ -35,16 +35,11 @@ def home():
 @app.post("/predict")
 def predict(data: IrisInput):
     # converte entrada para numpy array
-    input_array = np.array(
-        [
-            [
-                data.sepal_length,
-                data.sepal_width,
-                data.petal_length,
-                data.petal_width,
-            ]
-        ]
-    )
+    input_array = np.array([[data.sepal_length,
+                        data.sepal_width,
+                        data.petal_length,
+                        data.petal_width]])
+
 
     # aplica o mesmo scaler do treinamento
     input_scaled = scaler.transform(input_array)
@@ -53,4 +48,3 @@ def predict(data: IrisInput):
     prediction = model.predict(input_scaled)
 
     return {"classe_predita": int(prediction[0])}
-
